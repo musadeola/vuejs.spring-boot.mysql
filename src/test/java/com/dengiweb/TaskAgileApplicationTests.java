091b96b24d75d39ce5c2a3f62962dd525f4d7100
faruk <musadeola@gmail.com>
2023-07-02 13:52:06 -0400

    create back-end scaffold

---
+++ src/test/java/com/dengiweb/TaskAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dengiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAgileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
